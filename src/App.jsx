480afbb7040d4f5a290f5d53efc71b5f33fa6521
aleemer <aleemertariq@gmail.com>
2023-12-12 09:30:15 -0800

    Added a new file.

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        I like the designs on this!
+        I like the designs on this! They are nice!
       </p>
     </>
   )
