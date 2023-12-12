3447f5d86c3a98ab6bbc7adef17c427acc8a3578
aleemer <aleemertariq@gmail.com>
2023-12-12 08:54:11 -0800

    I tripled the count!

--- src/App.jsx
+++ src/App.jsx
@@ -18,15 +18,15 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count + 3)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          I'm tripling the count on each click!
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        I like the designs on this!
       </p>
     </>
   )
